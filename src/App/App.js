d1e6c34f117dcc1dddbdbf69f62a3b4aa048af2e
Cesarsk <luca.cesarano1@gmail.com>
2021-02-08 22:50:47 +0100

    Infinitum Start
    Starting with Infinitum

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from '../Navbar/Navbar'
+import Content from '../Content/Content'
+import Card from '../Card/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Content></Content>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card/Card.js
@@ -0,0 +1,9 @@
+import "./card.css";
+
+export default function Card() {
+  return (
+    <div className="Card">
+      <div className="box"></div>
+    </div>
+  );
+}
